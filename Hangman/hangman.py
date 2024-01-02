a411ecf58ecd36e52cdc8bb395d53f74dc7c2026
C2Meng <139868860+C2Meng@users.noreply.github.com>
2024-01-02 13:12:20 +0800

    Update hangman.py

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,1 +1,17 @@
+import pygame
+import os
 
+import random
+
+
+#terminal where game happens
+screen = pygame.display.set_mode((800,600))
+
+
+
+#making the bg stick until the user quits
+Running = True
+while Running:
+
+    screen.fill((0,0,0))
+    screen.blit(screen, (0,0))
