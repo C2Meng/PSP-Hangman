c135a9e7b15f537b20a812973c9ae41568eff6c0
Yang Yi Yap <155413974+YangYiYap@users.noreply.github.com>
2024-01-05 18:52:03 +0800

    Update list.py

--- Hangman/list.py
+++ Hangman/list.py
@@ -1,1 +1,26 @@
 
+import random
+
+#lists of categories from easy to expert
+easy_categories_list = ["animals", "fruits", "colors", "numbers"]
+intermediate_categories_list = ["countries", "occupations", "sports", "food"]
+difficult_categories_list = ["python", "html", "css"]
+expert_categories_list = ["data science","cybersecurity","software engineering","game development"]
+
+#lists from easy_categories_list (first level)
+animals_list = ["hippopotamus", "rhinoceros", "penguin", "elephant", "giraffe", "flamingo"] # hints = describe it with characteristics, show first alphabet...
+fruits_list = ["pineapple", "raspberry", "watermelon", "blackberry", "kiwi", "guava"] # hints = describe it with characteristics, show first alphabet...
+colors_list = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] # hints = rainbow
+numbers_list = ["twelve", "seventeen", "eleven", "nine", "ten", "fifty"] # hints = 0-50
+
+#lists from intermediate_categories_list (second level)
+countries_list = ["MALAYSIA", "SINGAPORE", "PHILIPPINES", "BRUNEI", "THAILAND", "VIETNAM"] # hints = southeast asia
+occupations_list = ["DENTIST", "NURSE", "PHARMACIST", "PHYSICIAN", "ENGINEER", "THERAPIST"] # hints = describe it with characteristics, show first alphabet...
+sports_list = ["SKIING", "VOLLEYBALL", "ARCHERY", "SNOOKER", "ICE HOCKEY", "MARTIAL ARTS"] # hints = describe it with characteristics, show first alphabet...
+food_list = ["CHOCOLATE MOUSSE", "DIM SUM", "ICE CREAM, "HOT DOG", "FRENCH FRIES", "ROTI CANAI"] # hints = describe it with characteristics, show first alphabet...
+
+
+
+
+
+                 
