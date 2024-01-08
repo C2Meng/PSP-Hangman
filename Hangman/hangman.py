6521d60d3fa992d861f09cb8567d2907b3de1ecf
C2Meng <139868860+C2Meng@users.noreply.github.com>
2024-01-08 16:16:42 +0800

    Update hangman.py

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -11,17 +11,18 @@ Background=pygame.image.load("assets/BG.png")
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption ("Hangman")
 
-#Status progression// images
-firekeeper_png = [1,2,3,4,5,6]
-firekeeper_png[0]= pygame.image.load("assets/firekeeper1.png") #1
-firekeeper_png[1]= pygame.image.load("assets/firekeeper2.png") #2
-firekeeper_png[2]= pygame.image.load("assets/firekeeper3.png") #3
-firekeeper_png[3]= pygame.image.load("assets/firekeeper4.png") #4
-firekeeper_png[4]= pygame.image.load("assets/firekeeper5.png") #5
-firekeeper_png[5]= pygame.image.load("assets/firekeeper6.png") #6
+#Status progression// images 
+status_png = [0,1,2,3,4,5]
+status_png[0] = pygame.image.load("assets/firekeeper1.png")
+status_png[1] = pygame.image.load("assets/firekeeper2.png")
+status_png[2] = pygame.image.load("assets/firekeeper3.png")
+status_png[3] = pygame.image.load("assets/firekeeper4.png")
+status_png[4] = pygame.image.load("assets/firekeeper5.png")  #make sure there is the corresponding pngs in the assets folder 
+status_png[5] = pygame.image.load("assets/firekeeper6.png")  #these can be made more efficient but no brain power atm
+
 
 #game
-firekeeper_status = 0 
+firekeeper_status = 3
 
 
 
@@ -29,11 +30,12 @@ firekeeper_status = 0
 running = True
 while running:
     pygame.display.update()
-    screen.blit(firekeeper_png[firekeeper_status],(150,100))
+    screen.blit(status_png[firekeeper_status],(150,100))
     clock.tick(FPS)
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             pygame.quit()
             running = False
+
 
