38d9bb274d10255123315791826a646a1e44649f
Yang Yi Yap <155413974+YangYiYap@users.noreply.github.com>
2024-01-09 23:40:46 +0800

    Update userdata.py

--- Hangman/userdata.py
+++ Hangman/userdata.py
@@ -1,6 +1,7 @@
 
 import shutil
 
+
 def create_user_profile():
     username = input("Enter your username: ")
     while True:
@@ -17,11 +18,14 @@ def create_user_profile():
         else:
             print("Invalid input. Please enter a valid command, yes or no.")
     student = student.lower()
+    global stu
+    stu = student
 
-    if student == "yes":
+    if stu == "yes":
         while True:
             student_id = input("Enter your Student ID: ")
             if student_id.isdigit() and len(student_id) == 10:
+
                 break
             else:
                 print("Invalid input. Please enter a valid Student ID.")
@@ -30,12 +34,11 @@ def create_user_profile():
         student_id = "Invalid"
         faculty = "Invalid"
 
-
     user_profile = {
         "username": username,
         "age": age,
         "gender": gender,
-        "MMU student": student,
+        "MMU student": stu,
         "Student ID": student_id,
         "faculty": faculty
     }
@@ -50,8 +53,10 @@ def display_user_profile(user_profile):
     print(f"Age: {user_profile["age"]}".ljust(console_width))
     print(f"Gender: {user_profile["gender"]}".ljust(console_width))
     print(f"MMU student: {user_profile["MMU student"]}".ljust(console_width))
-    print(f"Student ID: {user_profile["Student ID"]}".ljust(console_width))
-    print(f"Faculty: {user_profile["faculty"]}".ljust(console_width))
+    if stu == "yes":
+        print(f"Student ID: {user_profile["Student ID"]}".ljust(console_width))
+        print(f"Faculty: {user_profile["faculty"]}".ljust(console_width))
+
 
 def main():
     # Create a user profile using user input
@@ -59,5 +64,6 @@ def main():
 
     # Display the user profile information [f(x) = y]
     display_user_profile(user_profile)
+
 
 main()
