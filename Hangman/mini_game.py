714c592204ae3ac835f10c9f6b49c95158dc305d
winnielee2005 <155413518+winnielee2005@users.noreply.github.com>
2024-01-15 15:18:14 +0800

    Update mini_game.py

--- Hangman/mini_game.py
+++ Hangman/mini_game.py
@@ -11,12 +11,16 @@ white = (500, 500, 500)
 black = (0, 0, 0)
 font_size = 35
 
+# win and lose
+win_msg = "Congratulations! Here's ya hint"
+lose_msg = "Oy tough luck! Try again next time"
+
 # set up display
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_captions("Welcome to the typing challenge")
+pygame.display.set_caption("Welcome to the typing challenge")
 clock = pygame.time.Clock()
 
-# to generate the random words
+# to generate the random words ( will change the words later )
 def generate_word():
     word_list = ["meow", "woof", "moo"]
     return random.choice(word_list)
@@ -27,5 +31,24 @@ def main():
     current_word = generate_word()
 
 # loop for game
-    while 
+for event in pygame.event.get():
+    if event.type == pygame.QUIT
+       pygame.quit()
+       sys.exit()
+   elif event.type == pygame.KEYDOWN:
+       if event.key == pygame.K_RETURN:
+           if user_input == curent_word:
+               message1 = font.render(win_msg, black)
+           else:
+               message2 = font.render(lose_msg, black)
+               user_input = ""
+
+# render user input
+input_surface = font.render(user_input, black)
+input_rect = input_surface.get.rect(center=(width // 2, height // 2 ))
+screen.blit(input_surface, input_rect)
+
+
+
+        
 
