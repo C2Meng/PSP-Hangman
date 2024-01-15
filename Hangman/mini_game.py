90cb6f2ba23fa95de0ccc0a2f6a97004f9c0fe4d
winnielee2005 <155413518+winnielee2005@users.noreply.github.com>
2024-01-15 20:34:37 +0800

    Update mini_game.py

--- Hangman/mini_game.py
+++ Hangman/mini_game.py
@@ -6,8 +6,8 @@ import random
 pygame.init()
 
 # display variables
-width, height = 200
-white = (500, 500, 500)
+width, height = 400, 500
+white = (255, 255, 255)
 black = (0, 0, 0)
 font_size = 35
 
@@ -20,34 +20,52 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Welcome to the typing challenge")
 clock = pygame.time.Clock()
 
+# font
+font = pygame.font.Font(None, 36)
+
 # to generate the random words ( will change the words later )
 def generate_word():
     word_list = ["meow", "woof", "moo"]
     return random.choice(word_list)
-
-# to output the random word
-def main():
-    user_input = " "
-    current_word = generate_word()
+    
+# how many times to play
+turns = 4
+current_turn = 1
 
 # loop for game
+while current_turn < turns:
+    current_turn = current_turn + 1
+
+user_input = ""
+current_word = generate_word()
+
 for event in pygame.event.get():
-    if event.type == pygame.QUIT
-       pygame.quit()
-       sys.exit()
-   elif event.type == pygame.KEYDOWN:
-       if event.key == pygame.K_RETURN:
-           if user_input == curent_word:
-               message1 = font.render(win_msg, black)
+       if event.type == pygame.QUIT
+           pygame.quit()
+           sys.exit()
+       elif event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_RETURN:
+               if user_input == curent_word:
+                  message1 = font.render(win_msg, black)
+               else:
+                  message2 = font.render(lose_msg, black)
+                  user_input = ""
+           elif event.key == pygame.K_BACKSPACE:
+               user_input = user_input[:-1]
            else:
-               message2 = font.render(lose_msg, black)
-               user_input = ""
+               user_input += event.unicode
+           
+screen.fill(white)
 
 # render user input
 input_surface = font.render(user_input, black)
-input_rect = input_surface.get.rect(center=(width // 2, height // 2 ))
+input_rect = input_surface.get_rect(center=(width // 2, height // 2 ))
 screen.blit(input_surface, input_rect)
 
+# render target text
+target_surface = font.render(current_word, black)
+target_rect = target_surface.get_rect(center=(width // 2, height // 4 ))
+screen.blit(target_surface, target_rect)
 
 
         
